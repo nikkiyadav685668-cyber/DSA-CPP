b316f9a8073d61ee05a95dd253e420e968da637f
nikkiyadav685668-cyber <nikkiyadav685668@gmail.com>
2026-08-24 23:07:13 +0530

    Time: 5 ms (18.6%), Space: 8.6 MB (36.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -14,4 +14,20 @@ public:
 
         return x == reversedHalf || x == reversedHalf / 10;
     }
+    // int rev =0;
+    // int rem ;
+    // int temp = n;
+    // while(n > 0){
+    //     rem = n % 10;
+    //     rev = (rev * 10) + rem;
+    //     n = n / 10;
+
+    // }
+    // if(temp ==rev){
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // // }
+    // }
 };
